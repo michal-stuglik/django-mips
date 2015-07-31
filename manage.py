24e10f27967d40ec5c5baf2556454bf2bf864a7f
Michał Stuglik <stuglik@gmail.com>
2015-07-31 18:44:09 +0200

    Update manage.py

--- manage.py
+++ manage.py
@@ -4,10 +4,10 @@ import sys
 
 if __name__ == "__main__":
 
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mips.settings")
+    
     import django
     django.setup()
-
-    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mips.settings")
-
+    
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
