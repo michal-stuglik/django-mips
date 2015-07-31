17c9d8073ef169f7fafa3ba3ffeb7095c7c05315
michal-stuglik <stuglik@gmail.com>
2015-07-31 11:44:15 +0200

    - more test initials

--- mips/settings.py
+++ mips/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
     # 'django.contrib.sessions',
     # 'django.contrib.messages',
     'django_nose',
+    'coverage',
 ]
 
 MIDDLEWARE_CLASSES = (
--- mips/tests.py
+++ mips/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
+from models import Mip
 
 # Create your tests here.
+
+
+# models test
+class MipTest(TestCase):
+
+    def create_mip(self, mip_id=1):
+        return Mip.objects.create(mip_id=mip_id)
+
+    def test_mip_creation(self):
+        w = self.create_mip()
+        self.assertTrue(isinstance(w, Mip))
+        self.assertEqual(w.__unicode__(), w.title)
+
+
