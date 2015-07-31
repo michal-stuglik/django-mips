e83be76b7d5fc440ff8a68f8752597fbe3db918f
michal-stuglik <stuglik@gmail.com>
2015-07-31 21:40:05 +0200

    - more tests

--- mips/tests.py
+++ mips/tests.py
@@ -8,10 +8,10 @@ os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mips.settings")
 django.setup()
 
 from django.test import TestCase
-from models import Mip
+from models import Mip, Instance
+import datetime
 
 
-# models test
 class MipTestCase(TestCase):
     def setUp(self):
         o = Mip.objects.create(mip_id=1, mip_start=1, mip_stop=2)
@@ -20,3 +20,18 @@ class MipTestCase(TestCase):
     def test_mip_id_repr(self):
         mip1 = Mip.objects.get(mip_id=1)
         self.assertEqual(mip1.__unicode__(), u'1')
+
+
+class InstanceTestCase(TestCase):
+    def setUp(self):
+        m = Mip.objects.create(mip_id=1, mip_start=1, mip_stop=2)
+        m.save()
+
+        o = Instance.objects.create(mip_fk=m, mip_pool=1, mip_instance=1, mip_production_data=datetime.datetime.now())
+        o.save()
+
+    def test_instance_repr(self):
+        m = Mip.objects.get(mip_id=1)
+        o = Instance.objects.get(mip_fk=m, mip_instance=1)
+
+        self.assertEqual(o.__unicode__(), u'mip|mip_instance: 1|1')
