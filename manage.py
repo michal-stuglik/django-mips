421e32e6c7c1b21099ede585d8465dbbb45b80ef
michal-stuglik <stuglik@gmail.com>
2015-07-31 18:46:30 +0200

    - Travis CI compatibility?

--- manage.py
+++ manage.py
@@ -5,9 +5,9 @@ import sys
 if __name__ == "__main__":
 
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mips.settings")
-    
+
     import django
     django.setup()
-    
+
     from django.core.management import execute_from_command_line
     execute_from_command_line(sys.argv)
