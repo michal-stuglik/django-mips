48e2f50cfac5be175d6150780bef8e9c8e999285
michal-stuglik <stuglik@gmail.com>
2015-11-13 14:45:41 +0000

    version change fix

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-mips',
-    version='0.2.0',
+    version='0.3.0',
     packages=['mips'],
     include_package_data=True,
     license='MIT License',
